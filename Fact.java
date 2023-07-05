507fbb8984c095183e743a1630835d50e8e6ead6
verma99vipul <v99vipul@gmail.com>
2023-07-05 11:46:31 +0530

    code added

---
+++ Fact.java
@@ -0,0 +1,48 @@
+public class Fact{
+	public static void main(String[] args) {
+		int n=8;
+		int fact=0;
+		for (int i=1;i<=10;i++ ) {
+			
+			fact += n*i;
+			
+		}
+		System.out.println( fact);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		// FACTORIAL PROGRAM
+        // int n=5;
+        // int factorial=1;
+        
+		// for (int i=1 ; i<=n;i++)
+		//  {
+        //       factorial *= i;
+			
+		// }
+		// System.out.print(factorial);
+
+
+
+	}
+}
+
+
+//5=5 4 3 2 1
